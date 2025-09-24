663033794843d36540e16868f8d9d379ce5bf04c
katassyn <mastu2115@gmail.com>
2025-09-24 18:04:32 +0200

    1

---
+++ src/main/java/pl/yourserver/bloodChestPlugin/BloodChestPlugin.java
@@ -0,0 +1,17 @@
+package pl.yourserver.bloodChestPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BloodChestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
